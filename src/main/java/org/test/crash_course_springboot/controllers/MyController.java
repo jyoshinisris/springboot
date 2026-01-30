f5b67f055684c26e37802e1190150626670a621c
jyoshinisris <jyoshinisris@gmail.com>
2026-01-30 19:21:53 +0530

    Implemented GET Mapping,Configured Aiven Cloud DB

---
+++ src/main/java/org/test/crash_course_springboot/controllers/MyController.java
@@ -0,0 +1,23 @@
+package org.test.crash_course_springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/add_two_numbers")
+    public int addTwoNumbers(
+            @RequestParam int a,
+            @RequestParam int b){
+        return a + b;
+    }
+
+    @GetMapping("/mul_two_numbers")
+    public int mulTwoNumbers(
+            @RequestParam int a,
+            @RequestParam int b){
+        return a * b ;
+    }
+}
---
+++ src/test/java/org/test/crash_course_springboot/CrashCourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.crash_course_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashCourseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
